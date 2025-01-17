b56c7d64be6839e08e3961e517f5dff809b1e5ba
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-17 14:35:34 +0100

    feat: add removehandler

--- src/assets/components/Form.jsx
+++ src/assets/components/Form.jsx
@@ -4,42 +4,57 @@ const list = [];
 
 const Form = () => {
   const [myList, setMyList] = useState(list);
-  const [newTask, setNewTask] = useState("");
+  const [newTask, setNewTask] = useState({ text: "" });
 
   const handlerSubmit = (e) => {
     e.preventDefault();
     setMyList([newTask, ...myList]);
   };
 
+  const handlerNewTask = (e) => {
+    const newTask = {
+      id: Date.now(),
+      text: e.target.value,
+    };
+    setNewTask(newTask);
+  };
+  const handlerRemove = (id) => {
+    const newList = myList.filter((item) => item.id !== id);
+    setMyList(newList);
+  };
+
   return (
     <>
-      <form action="#" onSubmit={handlerSubmit}>
-        <div className="container my-5">
+      <div className="container my-5">
+        <form action="#" onSubmit={handlerSubmit}>
           <div className="input-group">
             <input
               type="text"
               className="form-control"
               placeholder="Scrivi titolo da aggiungere..."
-              value={newTask}
-              onChange={(e) => setNewTask(e.target.value)}
+              value={newTask.text}
+              onChange={handlerNewTask}
             />
             <button className="btn btn-outline-secondary" type="submit">
               Aggiungi
             </button>
           </div>
-          <ul className="list-group">
-            {myList.map((task, index) => (
-              <li
-                key={index}
-                className="list-group-item  d-flex justify-content-between"
-              >
-                {task}
-                <i class="fa-solid fa-trash"></i>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </form>
+        </form>
+      </div>
+      <ul className="list-group">
+        {myList.map((task) => (
+          <li
+            key={task.id}
+            className="list-group-item  d-flex justify-content-between"
+          >
+            {task.text}
+            <i
+              className="fa-solid fa-trash"
+              onClick={() => handlerRemove(task.id)}
+            ></i>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };

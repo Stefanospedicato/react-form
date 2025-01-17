491b342e4f273e5c2d0afcbc9734a7747939c2cd
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-17 14:00:07 +0100

    feat: add additem rule

--- src/assets/components/Form.jsx
+++ src/assets/components/Form.jsx
@@ -1,21 +1,43 @@
-import { useState } from "react"
+import { useState } from "react";
+
+const list = [];
 
 const Form = () => {
+  const [myList, setMyList] = useState(list);
+  const [newTask, setNewTask] = useState("");
 
-
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    setMyList([newTask, ...myList]);
+  };
 
   return (
     <>
-      <form>
+      <form action="#" onSubmit={handlerSubmit}>
         <div className="container my-5">
           <div className="input-group">
-            <input type="text" className="form-control" placeholder="Scrivi titolo da aggiungere..." aria-label="Example text with button addon" aria-describedby="button-addon1" />
-            <button className="btn btn-outline-secondary" type="button" id="button-addon1">Aggiungi</button>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Scrivi titolo da aggiungere..."
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+            />
+            <button className="btn btn-outline-secondary" type="submit">
+              Aggiungi
+            </button>
           </div>
+          <ul className="list-group">
+            {myList.map((listItem, index) => (
+              <li key={index} className="list-group-item">
+                {listItem}
+              </li>
+            ))}
+          </ul>
         </div>
       </form>
     </>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
